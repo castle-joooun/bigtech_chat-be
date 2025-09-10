ba0220a3a7cad524f7828df709299dee7bbfc9ac
castle-joooun <tjdwns0309@gmail.com>
2025-09-10 10:58:01 +0900

    UPDATE: delete room_members

--- app/models/room_members.py
+++
@@ -1,5 +0,0 @@
-# NOTE: This file is deprecated in favor of group_room_members.py
-# 1:1 chat rooms (chat_rooms table) don't need separate membership tracking
-# since membership is implicit from user_id_1 and user_id_2 in the chat_rooms table
-# 
-# This file is kept for potential future use or migration purposes
